cdeb0b0544698ef4278f44d921bca70a18d3b212
Julia <veluat@gmail.com>
2024-12-20 01:07:56 +0300

    init

--- src/js/main.js
+++ src/js/main.js
@@ -1,21 +1,43 @@
 import '../scss/styles.scss'
-import 'bootstrap';
+import 'bootstrap'
 
-window.addEventListener('load', function() {
-  const video = document.getElementById('backgroundVideo');
-  const image = document.getElementById('backgroundImage');
+document.addEventListener('DOMContentLoaded', function () {
+  const toggler = document.getElementById('menuToggle')
+  const fullscreenMenu = document.getElementById('fullscreenMenu')
+  const closeMenuButton = document.getElementById('closeMenu')
+  const video = document.getElementById('backgroundVideo')
+  const image = document.getElementById('backgroundImage')
+  const navLinks = document.querySelectorAll('.navbar-nav .nav-link')
 
+  // Функция для переключения меню
+  function toggleMenu() {
+    fullscreenMenu.classList.toggle('show')
+  }
+
+  // Обработчики для кнопок меню
+  toggler.addEventListener('click', toggleMenu)
+  closeMenuButton.addEventListener('click', toggleMenu)
+
+  // Проверка воспроизведения видео
   function checkVideoPlayback() {
     if (window.innerWidth < 768) {
-      video.style.display = 'none';
-      image.style.display = 'block';
+      video.style.display = 'none'
+      image.style.display = 'block'
     } else {
-      video.style.display = 'block';
-      image.style.display = 'none';
+      video.style.display = 'block'
+      image.style.display = 'none'
     }
   }
 
-  checkVideoPlayback();
+  // Инициализация проверки видео
+  checkVideoPlayback()
+  window.addEventListener('resize', checkVideoPlayback)
 
-  window.addEventListener('resize', checkVideoPlayback);
-});
+  // Обработчик навигационных ссылок
+  navLinks.forEach(link => {
+    link.addEventListener('click', function () {
+      navLinks.forEach(nav => nav.classList.remove('active'))
+      this.classList.add('active')
+    })
+  })
+})
